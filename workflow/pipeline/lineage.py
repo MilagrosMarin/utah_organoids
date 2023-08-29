59e4d55c53ff130ff4a63caf42cf4aa1b6abc261
Kabilar Gunalan <kabilar@datajoint.com>
2023-08-29 13:25:44 -0500

    Update lineage schema

--- workflow/pipeline/lineage.py
+++ workflow/pipeline/lineage.py
@@ -9,20 +9,25 @@ schema = dj.schema(DB_PREFIX + "lineage")
 
 
 @schema
-class Lineage(dj.Manual):
+class Storage(dj.Manual):
     definition = """
     induction_id: varchar(8)     # de-identified code (e.g. hmau001)
     ---
+    storage_position: enum(1,2,3,4,5)
+    stand: int unsigned
+    rack: int unsigned
+    box_position: int unsigned
+    frozen_date: date
     family: varchar(8)
     line: varchar(8)
-    passage_id: int unsigned
+    passage: int unsigned
     lineage_note='': varchar(256)
     """
 
 
 @schema
-class LineageSequence(dj.Manual):
+class Sequence(dj.Manual):
     definition = """
-    -> Lineage
+    -> Storage
     sequence: varchar(32)
     """
