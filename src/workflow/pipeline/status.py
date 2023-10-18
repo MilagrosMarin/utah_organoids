4bec6edbb306bb111c9f1a55a9d20b00d69011ca
JaerongA <jaerong.ahn@datajoint.com>
2023-10-18 18:27:04 -0500

    chore: :fire: remove status.py

--- src/workflow/pipeline/status.py
+++
@@ -1,9 +0,0 @@
-from workflow.pipeline import ephys, session
-
-
-def get_session_status():
-    """Determine which tables have been autopopulated.
-    Returns:
-        A joined table indicating the number of entries several database tables starting at the session level.
-    """
-    pass
