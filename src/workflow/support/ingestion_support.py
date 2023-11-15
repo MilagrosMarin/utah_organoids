2fe6add8c2e5ae9a2701a9042e74593cf6fff300
JaerongA <jaerong.ahn@datajoint.com>
2023-11-15 13:58:16 -0600

    refactor: :art: update to use FileProcessing

--- src/workflow/support/ingestion_support.py
+++ src/workflow/support/ingestion_support.py
@@ -32,9 +32,8 @@ class FileProcessing(dj.Imported):
         log_message = ""
         remote_fullpath = Path(key["remote_fullpath"])
         if Path(REL_PATH_INBOX) in remote_fullpath.parents:
-            subject_key = {}
             parent_dir = remote_fullpath.parent
-            if remote_fullpath.suffix == ".rhs":
+            if remote_fullpath.suffix in [".rhd", ".rhs"]:
                 filename_prefix, start_time = re.search(
                     r"(.*)_(\d{6}_\d{6})", remote_fullpath
                 ).groups()
@@ -43,9 +42,11 @@ class FileProcessing(dj.Imported):
                 )  # start time based on the file name
                 ephys.EphysRawFile.insert1(
                     {
-                        **subject_key,
-                        "file_time": start_time,
                         "file_path": remote_fullpath.as_posix(),
+                        "acq_software": {".rhd": "Intan", ".rhs": "Intan"}[
+                            remote_fullpath.suffix
+                        ],
+                        "file_time": start_time,
                         "parent_folder": parent_dir.name,
                         "filename_prefix": filename_prefix,
                         "file": (FileManifest & key).fetch1("file"),
@@ -62,44 +63,6 @@ class FileProcessing(dj.Imported):
                     < file_count
                 ):
                     return
-                # Attempt to create entries for ephys.EphysSession - the logic is as follows:
-                # For all the files in the uploaded folder, create session(s) that is at most 1-hour long
-                sess_config = {
-                    "insertion_number": 0,
-                    "probe": "111",
-                    "electrode_config_hash": "f2b8cfac-94cd-2564-da65-60493543f043",
-                    "session_type": "lfp",
-                }
-
-                session_duration = timedelta(hours=1)
-                file_times = (
-                    ephys.EphysRawFile & {"parent_folder": parent_dir.name}
-                ).fetch("file_time", order_by="file_time")
-
-                session_entries = []
-                start = file_times.pop(0)
-                previous = start
-                while file_times:
-                    current = file_times.pop(0)
-                    if (current - start) >= session_duration:
-                        session_entries.append((start, previous))
-                        start = current
-                    previous = current
-                session_entries.append((start, previous))
-                log_message += (
-                    f"Creating {len(session_entries)} ephys session(s) for {subject_key}: {session_entries}"
-                    + "\n"
-                )
-
-                ephys.EphysSession.insert(
-                    {
-                        **subject_key,
-                        **sess_config,
-                        "start_time": start,
-                        "end_time": end,
-                    }
-                    for start, end in session_entries
-                )
 
         self.insert1(
             {**key, "execution_time": datetime.utcnow(), "log_message": log_message}
